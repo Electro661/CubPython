21c0f5273d607f29d69b86eedbe626dc09c080c4
Electro661 <82703460+Electro661@users.noreply.github.com>
2021-04-17 16:30:11 +0700

    Update Cubgame.py

--- Cubgame.py
+++ Cubgame.py
@@ -1,6 +1,8 @@
 # Coding Program progressing
 import random #biblio random hes imported
 what = input("You were told by a friend of Max that if you throw a die and you have the number 6, he will give you a hundred (which he owes you")# 
+
 #the meaning of the game is made
 print (random.randint(1,6 ) )
+input()
 # this is code coding for fun
